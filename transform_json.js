eb69162a0cfe10398a27e8f0571b34270b19f342
Florian Quattrocchi <florianquattrocchi@gmail.com>
2017-04-03 15:15:25 +0200

    Huge file stream reading

--- transform_json.js
+++ transform_json.js
@@ -72,3 +72,22 @@ sql.connect(config).then(function() {
   console.log("CONNECTION ERROR".red);
   console.log(err);
 });
+// Pour lire des larges fichiers
+// var fs = require('fs');
+// var readline = require('readline');
+// var stream = require('stream');
+//
+// var instream = fs.createReadStream('historics_json.json');
+// var outstream = new stream;
+// var rl = readline.createInterface(instream, outstream);
+//
+// rl.on('line', function(line) {
+//   // process line here
+//   console.log(line);
+// });
+//
+// rl.on('close', function() {
+//   // do something on finish here
+//   console.log("close");
+// });
+//
